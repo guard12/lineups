549699416796f10560af9059fabf1e435fa24db7
Matus Kollarcik <mko@coinify.com>
2024-11-06 20:57:01 +0100

    Try to fix theme button

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -28,7 +28,7 @@ export const Navbar = () => {
         </Link>
       </NavigationMenu>
       <Button variant="outline" size="icon" onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
-        {theme === 'dark' && (
+        {(!theme || theme === 'dark') && (
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100" />
         )}
         {theme === 'light' && (
